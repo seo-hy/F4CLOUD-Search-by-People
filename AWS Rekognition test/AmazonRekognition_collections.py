0a73d7e52a31fe7e7fa6ef2332afa8d6ed34bf5a
seoh02h <seok02h@khu.ac.kr>
2021-04-05 09:57:30 +0900

    AWS test

---
+++ AWS Rekognition test/AmazonRekognition_collections.py
@@ -0,0 +1,75 @@
+import boto3
+from botocore.exceptions import ClientError
+from os import environ
+
+def create_collection(collection_id):
+
+    client=boto3.client('rekognition')
+
+    #Create a collection
+    print('Creating collection:' + collection_id)
+    response=client.create_collection(CollectionId=collection_id)
+    print('Collection ARN: ' + response['CollectionArn'])
+    print('Status code: ' + str(response['StatusCode']))
+    print('Done...')
+
+
+def list_collections():
+    max_results = 2
+
+    client = boto3.client('rekognition')
+
+    # Display all the collections
+    print('Displaying collections...')
+    response = client.list_collections(MaxResults=max_results)
+    collection_count = 0
+    done = False
+
+    while done == False:
+        collections = response['CollectionIds']
+
+        for collection in collections:
+            print(collection)
+            collection_count += 1
+        if 'NextToken' in response:
+            nextToken = response['NextToken']
+            response = client.list_collections(NextToken=nextToken, MaxResults=max_results)
+
+        else:
+            done = True
+
+    return collection_count
+
+
+def delete_collection(collection_id):
+    print('Attempting to delete collection ' + collection_id)
+    client = boto3.client('rekognition')
+    status_code = 0
+    try:
+        response = client.delete_collection(CollectionId=collection_id)
+        status_code = response['StatusCode']
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print('The collection ' + collection_id + ' was not found ')
+        else:
+            print('Error other than Not Found occurred: ' + e.response['Error']['Message'])
+        status_code = e.response['ResponseMetadata']['HTTPStatusCode']
+    return (status_code)
+
+def main():
+    #Create a collection
+    #collection_id='<collection_id>'
+    #create_collection(collection_id)
+
+    #List Collections
+    collection_count=list_collections()
+    print("collections: " + str(collection_count))
+
+    #Delete a collection
+    #collection_id = '<collection_id>'
+    #status_code = delete_collection(collection_id)
+    #print('Status code: ' + str(status_code))
+
+if __name__ == "__main__":
+    main()
---
+++ AWS Rekognition test/AmazonRekognition_list_faces.py
@@ -0,0 +1,39 @@
+import boto3
+
+
+def list_faces_in_collection(collection_id):
+    maxResults = 2
+    faces_count = 0
+    tokens = True
+
+    client = boto3.client('rekognition')
+    response = client.list_faces(CollectionId=collection_id,
+                                 MaxResults=maxResults)
+
+    print('Faces in collection ' + collection_id)
+
+    while tokens:
+
+        faces = response['Faces']
+
+        for face in faces:
+            print(face)
+            faces_count += 1
+        if 'NextToken' in response:
+            nextToken = response['NextToken']
+            response = client.list_faces(CollectionId=collection_id,
+                                         NextToken=nextToken, MaxResults=maxResults)
+        else:
+            tokens = False
+    return faces_count
+
+
+def main():
+    collection_id = '<collection_id>'
+
+    faces_count = list_faces_in_collection(collection_id)
+    print("faces count: " + str(faces_count))
+
+
+if __name__ == "__main__":
+    main()
