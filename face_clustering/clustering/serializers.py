63d2609178ec2b31b22852a2ff3a94f99916d79c
Seohyun <seok02h@khu.ac.kr>
2021-05-21 00:07:26 +0900

    Delete face_clustering directory

--- face_clustering/clustering/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from clustering.models import FileInfo
-
-class AddFaceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FileInfo
-        fields = ['file_address']
-
